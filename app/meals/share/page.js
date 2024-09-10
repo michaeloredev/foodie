29b14ac0696b62946ebdcb122e942141458cc4a3
more0210 <michael.ore.dev@gmail.com>
2024-09-09 21:22:49 -0400

    server actions and image upload

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,7 +1,27 @@
-import classes from './page.module.css';
-import ImagePicker from '@/components/meals/image-picker';
+import classes from "./page.module.css";
+import ImagePicker from "@/components/meals/image-picker";
+import { shareMeal } from "@/lib/actions";
 
 export default function ShareMealPage() {
+
+  // async function shareMeal(formData) {
+  //   // This creates a server action, which is garunteed to be executed on the server.  'async' is required for this to work.
+  //   // formData should be passed to handle submitted data
+  //   // !! THIS ONLY WORKS IN A NON 'use client' PAGE !!
+  //   "use server";
+
+  //   const meal = {
+  //     creator: formData.get("name"),
+  //     creator_email: formData.get("email"),
+  //     title: formData.get("title"),
+  //     summary: formData.get("summary"),
+  //     instructions: formData.get("instructions"),
+  //     image: formData.get("image"),
+  //   };
+
+  //   console.log(meal);
+  // }
+
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +31,8 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        {/* This action will be executed on the server, this is noted above*/}
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,7 +60,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your Image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
