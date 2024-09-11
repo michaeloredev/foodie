39e7aba6da2f61b01db335cbd7802d7f6073038a
more0210 <michael.ore.dev@gmail.com>
2024-09-10 22:00:50 -0400

    form validation, cache re validation, ux updates

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,8 +1,13 @@
+'use client';
+
 import classes from "./page.module.css";
 import ImagePicker from "@/components/meals/image-picker";
 import { shareMeal } from "@/lib/actions";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
+import { useFormState } from 'react-dom';
 
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, {message:null} )
 
   // async function shareMeal(formData) {
   //   // This creates a server action, which is garunteed to be executed on the server.  'async' is required for this to work.
@@ -32,7 +37,7 @@ export default function ShareMealPage() {
       </header>
       <main className={classes.main}>
         {/* This action will be executed on the server, this is noted above*/}
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -62,7 +67,8 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your Image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            {state.message && <p>{state.message}</p>}
+          <MealsFormSubmit />
           </p>
         </form>
       </main>
